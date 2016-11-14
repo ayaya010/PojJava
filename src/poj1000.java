a9ddd767519cd29c60ffac22d8c06fef6cf99cc9
wanglei <wanglei@DESKTOP-2O8ALLP>
2016-11-14 15:09:27 +0800

    poj1000

--- src/poj1000.java
+++ src/poj1000.java
@@ -1,4 +1,10 @@
 import java.util.*;
 public class poj1000 {
-
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a + b);
+        in.close();
+        }
 }
